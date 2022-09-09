0b062187af1560db1d21d67df99ff5b1b47c8d4e
sarita devkar <devkarsarita012@gmail.com>
2022-09-09 07:21:30 +0530

    add link component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -16,15 +17,23 @@ export default function Home() {
 
        <nav className={styles.mainnav}>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Blog</li>
+        <Link href="/">
+          <a><li>Home</li> </a>
+        </Link>
+          <Link href="/about">
+          <a><li>About</li> </a>
+        </Link>
+          <Link href="/contact">
+          <a><li>Contact</li></a>
+        </Link>
+          <Link href="/blog">
+          <a><li>Blog</li></a>
+        </Link>
         </ul>
        </nav>
       <main className={styles.main}>
         <h1 className={styles.title}>
-         Coder
+         Coding with Sarita
         </h1>
 
         <p className={styles.description}>
